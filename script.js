736b3a2ddf85afe57287abb3d4965a0cbd729736
Filipe Santos <outerspaceguy@live.co.uk>
2022-08-25 14:04:36 -0300

    decodificador

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-function alerta() {
-    alert("Oiiiii");
-  
+function jogartextoarea2() {
+  var texto1 = document.querySelector("#textarea1")
+  var area2 = document.querySelector("#textarea2")
+  area2.value = texto1.value
 }
 
 
 function copiar() {
-
-var mensagem = document.querySelector("#textarea2");
-mensagem.select();
-navigator.clipboard.writeText(mensagem.value);
-
-}
+    area2.select();
+    navigator.clipboard.writeText(area2.value)
+    }
