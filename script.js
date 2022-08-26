0f2670b348802882ec737520e0c05ab4c915c235
Filipe Santos <outerspaceguy@live.co.uk>
2022-08-26 14:08:35 -0300

    decodificador

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-function jogartextoarea2() {
-  var texto1 = document.querySelector("#textarea1")
-  var area2 = document.querySelector("#textarea2")
-  area2.value = texto1.value
+function encriptar() {
+  var inputText = document.querySelector("#texto-entrada");
+  var outputText = document.querySelector("#texto-saida");
+
+  var texto = inputText.value.toLowerCase();
+  
+  var textoCriptografado = texto
+  .replace(/e/g,"enter")
+  .replace(/i/g,"imes")
+  .replace(/a/g,"ai")
+  .replace(/o/g,"ober")
+  .replace(/u/g,"ufat");
+
+  outputText.value = textoCriptografado;
+}
+function descriptografar() {
+  var inputText = document.querySelector("#texto-entrada");
+  var outputText = document.querySelector("#texto-saida");
+
+  var texto = inputText.value.toLowerCase();
+
+  var textoDescriptografado = texto
+  .replace(/enter/g,"e")
+  .replace(/imes/g,"i")
+  .replace(/ai/g,"a")
+  .replace(/ober/g,"o")
+  .replace(/ufat/g,"u")
+
+  outputText.value = textoDescriptografado
 }
 
 
+
 function copiar() {
-    area2.select();
-    navigator.clipboard.writeText(area2.value)
+    var outputText = document.querySelector("#texto-saida");
+    outputText.select();
+    navigator.clipboard.writeText(outputText.value);
+    outputText.value = "";
     }
